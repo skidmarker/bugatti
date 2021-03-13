99c609dd00d6067ef2fe255edcfd9a73d2ee07f3
Sowon Jung <ss_won_@naver.com>
2021-03-13 22:07:56 +0900

    solve: programmers #42583, #42584, #42586, #42587

---
+++ stack+queue/42584.py
@@ -0,0 +1,10 @@
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices) - 1, -1, -1):
+        cnt = 0
+        for j in range(i + 1, len(prices)):
+            cnt += 1
+            if prices[i] > prices[j]:
+                break
+        answer[i] = cnt
+    return answer
