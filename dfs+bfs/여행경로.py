26e828a625ea9b4fc7375d12968859f505e7a8bd
Sowon Jung <swj960515@gmail.com>
2021-04-18 21:16:12 +0900

    solve: programmers 여행경로 코드 수정

--- dfs+bfs/여행경로.py
+++ dfs+bfs/여행경로.py
@@ -1,25 +1,29 @@
 finish = False
+use = 0
 
 
 def solution(tickets):
     answer = []
-    visited = []
+    visited = [False] * len(tickets)
     tickets.sort()
 
-    def dfs(cur, finish):
-        if len(visited) == len(tickets):
+    def dfs(cur):
+        global finish
+        global use
+        if use == len(tickets):
             finish = True
             return
-        for id in range(len(tickets)):
-            if tickets[id][0] == cur and not (id in visited):
-                visited.append(id)
-                answer.append(tickets[id][1])
-                dfs(tickets[id][1], finish)
-                if finish == False:
-                    visited.remove(id)
-                    answer.remove(tickets[id][1])
-                print(answer)
+        for i in range(len(tickets)):
+            if tickets[i][0] == cur and not visited[i]:
+                visited[i] = True
+                answer.append(tickets[i][1])
+                use += 1
+                dfs(tickets[i][1])
+                if not finish:
+                    visited[i] = False
+                    use -= 1
+                    answer.pop()
 
     answer.append("ICN")
-    dfs("ICN", False)
+    dfs("ICN")
     return answer
